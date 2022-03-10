4ace874e53f27203193d3ba54b97253bafbf1273
willberOdoo <willrr.payall.2021@gmail.com>
2022-03-10 15:35:15 +0000

    [FIX] save information of the invoice

--- ve_tax_withholding/models/account_move.py
+++ ve_tax_withholding/models/account_move.py
@@ -2,5 +2,5 @@
 from odoo import models, fields, api
 class AccountMove( models.Model):
     _inherit = 'account.move'
-    control_number = fields.Char(string='Numero de control', required=True, default='00')
+    control_number = fields.Char(string='Numero de control', required=True, store=True default='00')
     
