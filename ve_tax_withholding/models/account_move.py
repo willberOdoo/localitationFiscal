e9fd06326f89f087ca7194fdf42965459d2872d9
Will <odoo@eupd3.odoo.com>
2022-03-16 20:30:54 +0000

    [fix] required fiels subjects

--- ve_tax_withholding/models/account_move.py
+++ ve_tax_withholding/models/account_move.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 class AccountMove( models.Model):
     _inherit = 'account.move'
-    control_number = fields.Char(string='Numero de control', required=True, store=True, default='00') 
+    control_number = fields.Char(string='Numero de control',store=True, default='00') 
     
-    subjects = fields.Many2one( string = 'Concepto Retención ISLR', comodel_name='tax.withholding_subject', required = True) 
+    subjects = fields.Many2one( string = 'Concepto Retención ISLR', store=True, comodel_name='tax.withholding_subject') 
     
